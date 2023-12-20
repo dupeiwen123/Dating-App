8770e68bdd031367c27a95a24aefe4951f50a8f6
alexanderrichter99 <alexrichter0707@gmail.com>
2023-12-20 11:31:30 +0100

    add logic duration

--- public/load.js
+++ public/load.js
@@ -18,32 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
     startButton.addEventListener("click", () => {
         startButton.disabled = true;
         startButton.textContent = "Processing...";
-    
-        simulateEyeTrackingResults(10000); // 10000ms equals 10 seconds
-    
+
+        const duration = 10000; // Total duration in milliseconds
+        simulateEyeTrackingResults(duration);
+
         setTimeout(() => {
-            const eyeTrackingResults = simulateEyeTrackingResults(10000);
+            const eyeTrackingResults = simulateEyeTrackingResults(duration);
             displayResults(eyeTrackingResults);
             resultScreen.style.display = "block";
-        }, 10000); // 10 seconds for displaying results
+        }, duration);
     });
 
     // Simulates eye tracking results for given duration
     function simulateEyeTrackingResults(duration) {
-        console.log("Eye tracking running for 10 seconds...");
+        console.log(`Eye tracking running for ${duration / 1000} seconds...`);
 
         const eyeTrackingResults = [];
+        let totalDuration = 0;
 
-        // Simulating random gaze durations for each of the 9 images
+        // Simulating random gaze durations for each of the 9 images to match the total duration
         for (let i = 1; i <= 9; i++) {
-            const randomDuration = Math.floor(Math.random() * duration); // Random duration up to the provided time (in milliseconds)
+            const maxDuration = duration - totalDuration - (9 - i); // Ensure the total is accurate and distribute the time among images
+            const randomDuration = Math.floor(Math.random() * maxDuration); // Random duration up to the remaining time
             eyeTrackingResults.push({ photo: `Pic ${i}`, duration: randomDuration });
+            totalDuration += randomDuration;
         }
 
-        // Sort results in descending order based on gaze duration
-        eyeTrackingResults.sort((a, b) => b.duration - a.duration);
+        // Calculate the remaining duration for the last image to match the total duration
+        eyeTrackingResults[8].duration = duration - totalDuration;
 
-        // Output sorted results
+        // Output results
         eyeTrackingResults.forEach(result => {
             console.log(`Image: ${result.photo} - Gaze duration: ${result.duration}ms`);
         });
@@ -55,14 +59,14 @@ document.addEventListener("DOMContentLoaded", function () {
     function displayResults(eyeTrackingResults) {
         const resultDisplay = document.getElementById("resultDisplay");
         resultDisplay.innerHTML = "<h3>Gaze Duration of Images:</h3>";
-        
+
         eyeTrackingResults.forEach(result => {
             const resultElement = document.createElement("p");
             resultElement.textContent = `${result.photo} - Gaze duration: ${result.duration}ms`;
             resultDisplay.appendChild(resultElement);
         });
     }
-    
+
     // Initialize to show all photos
     fetchPhotos('all');
 
@@ -79,42 +83,42 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(allPhotos => {
                 // Remove existing photos
                 removeAllPhotos();
-    
+
                 const photosToShow = Math.min(9, allPhotos.length); // Limit the number of photos to a maximum of 9
-    
+
                 const photosContainer = document.createElement("div");
                 photosContainer.classList.add("photos-container");
-    
+
                 const columnCount = 3; // Number of columns
                 const photosPerColumn = Math.ceil(photosToShow / columnCount);
-    
+
                 for (let i = 0; i < columnCount; i++) {
                     const column = document.createElement("div");
                     column.classList.add("photo-column");
                     photosContainer.appendChild(column);
-    
+
                     for (let j = i * photosPerColumn; j < (i + 1) * photosPerColumn && j < photosToShow; j++) {
                         const photoUrl = allPhotos[j];
                         const photoElement = document.createElement("img");
                         photoElement.src = `photos/${gender}/${photoUrl}`;
                         photoElement.alt = `Photo ${j + 1}`;
                         photoElement.classList.add("photo");
-    
+
                         photoElement.addEventListener("click", () => {
                             displayLargePhoto(`photos/${gender}/${photoUrl}`);
                         });
-    
+
                         column.appendChild(photoElement);
                     }
                 }
-    
+
                 gallery.appendChild(photosContainer);
             })
             .catch(error => {
                 console.error(`Error fetching ${gender} photo list:`, error.message);
             });
     }
-    
+
     // Displays larger photo on click
     function displayLargePhoto(photoUrl) {
         largePhoto.src = photoUrl;
